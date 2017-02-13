7fe03dd77e5c1fea0bf5cb549048d9d3ad6b5241
saikishorereddy <saikishorereddy@CHINTU-PC>
2017-02-13 16:01:21 -0500

    Commit "Retreive data using Http"

--- Angular2.0PS/app/Products/product-list.component.ts
+++ Angular2.0PS/app/Products/product-list.component.ts
@@ -1,12 +1,14 @@
 ﻿import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
 import { ProductService } from "./product.service";
+
 @Component({
     selector: "pm-products",
     moduleId: module.id,
     templateUrl: "product-list.component.html",
     styleUrls: ["product-list.component.css"]
 })
+
 export class ProductListComponent implements OnInit{
     // pageTitle: string = "Product List";
     PageTitle: string = "Product List";
@@ -14,6 +16,7 @@ export class ProductListComponent implements OnInit{
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string;
+    errorMessage: string;
     products: IProduct[];
     constructor(private _productService: ProductService) {
 
@@ -22,7 +25,9 @@ export class ProductListComponent implements OnInit{
         this.showImage = !this.showImage;
     }
     ngOnInit(): void {
-        this.products = this._productService.getProducts();
+        this._productService.getProducts()
+            .subscribe(products => this.products = products,
+                        error => this.errorMessage = <any>error);
     }
 
     onRatingClicked(message: string): void {
--- Angular2.0PS/app/Products/product.service.ts
+++ Angular2.0PS/app/Products/product.service.ts
@@ -1,31 +1,26 @@
 ﻿import { Injectable } from '@angular/core';
 import { IProduct } from './product';
+import { Http, Response } from "@angular/http";
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 
 export class ProductService {
+    private _productUrl = 'app/products/products.json';
+    constructor(private _http: Http) {
 
-    getProducts(): IProduct[] {
-        return [
-            {
-                "productId": 1,
-                "productName": "Leaf Rake",
-                "productCode": "GDN-0011",
-                "releaseDate": "March 19, 2016",
-                "description": "Leaf rake with 48-inch wooden handle.",
-                "price": 19.95,
-                "starRating": 3.2,
-                "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-            },
-            {
-                "productId": 2,
-                "productName": "Garden Cart",
-                "productCode": "GDN-0023",
-                "releaseDate": "March 18, 2016",
-                "description": "15 gallon capacity rolling garden cart",
-                "price": 32.99,
-                "starRating": 4.4,
-                "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-            }];
+    }
+    getProducts(): Observable<IProduct[]> {
+        return this._http.request(this._productUrl)
+            .map((response: Response) => <IProduct[]>response.json())
+            .do(data => console.log('All: ' + JSON.stringify(data)))
+            .catch(this.handelError);
+    }
+    private handelError(error: Response) {
+        console.log(error);
+        return Observable.throw(error.json().error || 'Server error');
     }
 }
