fc0185c0923ae5706ded290ffe3f1b118cf265bb
saikishorereddy <saikishorereddy@CHINTU-PC>
2017-02-13 17:29:53 -0500

    Commit "Navigation and Routing"

--- Angular2.0PS/app/app.module.ts
+++ Angular2.0PS/app/app.module.ts
@@ -1,19 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from "@angular/forms";
-import {HttpModule} from "@angular/http"
+import { HttpModule } from "@angular/http";
+import { RouterModule } from "@angular/router";
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from "./home/welcome.component";
 import { ProductListComponent } from './products/product-list.component';
+import { ProductDetailComponent } from './products/product-details.component';
 import { ProductFilterPipe } from './products/product-filter.pipe';
 import { StarComponent } from './shared/star.component';
 @NgModule({
     imports: [BrowserModule,
         FormsModule,
-        HttpModule],
+        HttpModule,
+        RouterModule.forRoot([
+            { path: 'products', component: ProductListComponent },
+            { path: 'product/:id', component: ProductDetailComponent },
+            { path: 'welcome', component: WelcomeComponent },
+            { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+            { path: '**', redirectTo:'welcome', pathMatch:'full' }
+        ], { useHash: true })
+    ],
   declarations: [AppComponent,
         ProductListComponent,
         ProductFilterPipe,
-        StarComponent],
+        StarComponent,
+      WelcomeComponent,
+      ProductDetailComponent],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
