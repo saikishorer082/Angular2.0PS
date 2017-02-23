cccb6adde084373f4bbbc4663622dbbb421a7705
saikishorereddy <saikishorer082@gmail.com>
2017-02-23 11:46:10 -0500

    App Routing Module

--- Angular2.0PS/app/app.module.ts
+++ Angular2.0PS/app/app.module.ts
@@ -1,21 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from "@angular/http";
-import { RouterModule } from "@angular/router";
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from "./home/welcome.component";
 
 import { ProductModule } from "./products/product.module";
+import { AppRoutingModule } from "./app-routing.module";
 @NgModule({
     imports: [
         ProductModule,
         BrowserModule,
         HttpModule,
-        RouterModule.forRoot([
-            { path: 'welcome', component: WelcomeComponent },
-            { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-            { path: '**', redirectTo:'welcome', pathMatch:'full' }
-        ], { useHash: true })
+        AppRoutingModule
     ],
   declarations: [AppComponent,
       WelcomeComponent],
