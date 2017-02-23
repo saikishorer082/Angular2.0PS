c8d72d1f70d953d1bdb0121ebe4cfbd8597e06d2
saikishorereddy <saikishorer082@gmail.com>
2017-02-23 11:39:07 -0500

    Shared Module

--- Angular2.0PS/app/Products/product.module.js
+++ Angular2.0PS/app/Products/product.module.js
@@ -6,15 +6,13 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 var core_1 = require("@angular/core");
-var forms_1 = require("@angular/forms");
-var common_1 = require("@angular/common");
 var router_1 = require("@angular/router");
 var product_list_component_1 = require("./product-list.component");
 var product_details_component_1 = require("./product-details.component");
 var product_filter_pipe_1 = require("./product-filter.pipe");
-var star_component_1 = require("../shared/star.component");
 var product_guard_service_1 = require("./product-guard.service");
 var product_service_1 = require("./product.service");
+var shared_module_1 = require("../shared/shared.module");
 var ProductModule = (function () {
     function ProductModule() {
     }
@@ -26,11 +24,9 @@ ProductModule = __decorate([
             product_list_component_1.ProductListComponent,
             product_details_component_1.ProductDetailComponent,
             product_filter_pipe_1.ProductFilterPipe,
-            star_component_1.StarComponent
         ],
         imports: [
-            forms_1.FormsModule,
-            common_1.CommonModule,
+            shared_module_1.SharedModule,
             router_1.RouterModule.forChild([{ path: 'products', component: product_list_component_1.ProductListComponent },
                 {
                     path: 'product/:id',
--- Angular2.0PS/app/Products/product.module.ts
+++ Angular2.0PS/app/Products/product.module.ts
@@ -1,26 +1,21 @@
 ﻿import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 
 import { ProductListComponent } from "./product-list.component";
 import { ProductDetailComponent } from "./product-details.component";
 import { ProductFilterPipe } from "./product-filter.pipe";
-import { StarComponent } from "../shared/star.component";
 
 import { ProductDetailGuard } from "./product-guard.service";
 import { ProductService } from "./product.service";
-
+import { SharedModule } from "../shared/shared.module";
 @NgModule({
     declarations: [
         ProductListComponent,
         ProductDetailComponent,
         ProductFilterPipe,
-        StarComponent
     ],
     imports: [
-        FormsModule,
-        CommonModule,
+        SharedModule,
         RouterModule.forChild([ { path: 'products', component: ProductListComponent },
         {
             path: 'product/:id',
